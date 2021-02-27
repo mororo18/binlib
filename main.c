abb9623b9aee27f2a1a06b45538cca03fa8e1861
Victor Mororo <victormororo@gmail.com>
2021-02-27 18:46:55 -0300

    start presentation in main.c

--- main.c
+++ main.c
@@ -2,15 +2,21 @@
 #include "binlib.h"
 
 int main(){
-	bin_int32_t a = bin_int_assign("10000000000000000000000000001011");
-	bin_int8_t b = bin_int_assign("10000000");
-	bin_int8_t c;
+	bin_int32_t a = bin_int_assign("00000000000000000000000000001010");
+	bin_int16_t b = bin_int_assign("0000000000000101");
+	bin_int32_t c;
 
+	c = bin_int_add(a, b);      //return a pointer for a 32bit type ( 'a' is the bigger)
+
+    printf(" (A) %dbit binary : \t", a->sz);      bin_print(a);
+    printf(" (B) %dbit binary : \t\t\t", b->sz);  bin_print(b);
+    printf(" (C) %dbit binary : \t", c->sz);      bin_print(c);
+
+    bin_clear(&c);
     c = bin_from_int(127, 8);
+    bin_print(c);
     //printf("uint u %u\n", bin_to_uint(a));
     //printf("uint d %d\n", bin_to_int(a));
 	//bin_int_add_print(a, b);
-	//c = bin_int_add(a, b);
-    bin_print(c);
 	return 0;
 }
